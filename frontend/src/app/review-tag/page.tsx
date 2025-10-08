b0b8210a8b6f900e1b838096f98105368ef8e09e
Subham170 <deysubham3312@gmail.com>
2025-10-08 11:38:03 +0530

    updated the upload to IPFS modal

--- frontend/src/app/review-tag/page.tsx
+++ frontend/src/app/review-tag/page.tsx
@@ -4,7 +4,7 @@ import ReviewTagModal from "@/components/custom/review-tag-modal";
 
 export default function ReviewTagPage() {
   return (
-    <div className="p-8">
+    <div className="p-8 bg-[#181A1D]">
       <ReviewTagModal />
     </div>
   );
