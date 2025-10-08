0bfd0e67e951aac233a5a98a1c770ad62ed003e6
Subham170 <deysubham3312@gmail.com>
2025-10-08 11:42:26 +0530

    updated the review-tag page

--- frontend/src/app/review-tag/page.tsx
+++ frontend/src/app/review-tag/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
+import AuthenticatedLayout from "@/components/custom/layouts/authenticated-layout";
 import ReviewTagModal from "@/components/custom/review-tag-modal";
+import { useRouter } from "next/navigation";
 
 export default function ReviewTagPage() {
+  const router = useRouter();
+
+  const handleCancel = () => {
+    router.push("/create-tag");
+  };
+
   return (
-    <div className="p-8 bg-[#181A1D]">
-      <ReviewTagModal />
-    </div>
+    <AuthenticatedLayout>
+      <div className="min-h-screen flex items-center justify-center p-8 bg-[#181A1D]">
+        <ReviewTagModal onCancel={handleCancel} />
+      </div>
+    </AuthenticatedLayout>
   );
 }
